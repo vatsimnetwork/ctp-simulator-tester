eee2a8a67704f904ed90ffaa9ea79889b6fa28bc
vocux <github@sebastian-gruenewald.de>
2026-03-22 01:53:11 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using ConsoleTables;
+using CTPSimulator;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+
+namespace CTPSimulatorTester
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var vatsimEvent = TestingDataLoader.Load("25W");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            await SlotDistributionCreator.CreateSlotDistribution(vatsimEvent);
+            stopWatch.Stop();
+
+            // display the results
+            var table = new ConsoleTable("Departures", "NAT Tracks", "Arrivals");
+            table.Options.EnableCount = false;
+
+            var natSegments = vatsimEvent.RouteSegments.Where(r => r.RouteSegmentGroup == "NAT").ToList();
+            int rows = Math.Max(Math.Max(vatsimEvent.DepartureAirports.Count, natSegments.Count), vatsimEvent.ArrivalAirports.Count);
+            for (int i = 0; i < rows; i++)
+            {
+                table.AddRow(
+                    i < vatsimEvent.DepartureAirports.Count ? BuildInfo(vatsimEvent.DepartureAirports[i]) : string.Empty,
+                    i < natSegments.Count ? BuildInfo(natSegments[i]) : string.Empty,
+                    i < vatsimEvent.ArrivalAirports.Count ? BuildInfo(vatsimEvent.ArrivalAirports[i]) : string.Empty);
+            }
+
+            table.AddRow(
+                $"{vatsimEvent.DepartureAirports.Sum(da => da.SlotsAllocated)} / {vatsimEvent.DepartureAirports.Sum(da => da.MaximumSlots)}",
+                $"{natSegments.Sum(sr => sr.SlotsAllocated)} / {natSegments.Sum(sr => sr.MaximumSlots)}",
+                $"{vatsimEvent.ArrivalAirports.Sum(aa => aa.SlotsAllocated)} / {vatsimEvent.ArrivalAirports.Sum(aa => aa.MaximumSlots) }");
+
+            Console.WriteLine(table.ToString());
+
+            // display departure / arrival slots
+            table = new ConsoleTable([string.Empty, .. vatsimEvent.ArrivalAirports.Select(da => da.Identifier)]);
+            table.Options.EnableCount = false;
+
+            foreach (var departureAirport in vatsimEvent.DepartureAirports)
+            {
+                List<string> rowContent = [departureAirport.Identifier];
+                foreach (var arrivalAirport in vatsimEvent.ArrivalAirports)
+                {
+                    rowContent.Add(vatsimEvent.Slots.Count(s => s.DepartureAirport == departureAirport && s.ArrivalAirport == arrivalAirport).ToString());
+                }
+                table.AddRow(rowContent.ToArray());
+            }
+            Console.WriteLine(table.ToString());
+
+            Console.WriteLine($"Calculation took {stopWatch.ElapsedMilliseconds}ms");
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        static string BuildInfo(ThroughputPoint throughputPoint)
+        {
+            return $"{throughputPoint.Identifier} {throughputPoint.SlotsAllocated} / {throughputPoint.MaximumSlots}";
+        }
+    }
+}
---
+++ TestingDataLoader.cs
@@ -0,0 +1,63 @@
+﻿using CTPSimulator;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CTPSimulatorTester
+{
+    public static class TestingDataLoader
+    {
+        public static VATSIMEvent Load(string eventPrefix)
+        {
+            VATSIMEvent vatsimEvent = new() 
+            {
+                Title = eventPrefix
+            };
+
+
+            Dictionary<string, Location> locations = new();
+            Dictionary<string, Airport> airports = new();
+
+            // read airports
+            foreach (var line in File.ReadAllLines(Path.Combine("TestingData", $"{eventPrefix} Airports.csv")))
+            {
+                var splits = line.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                if (splits.Length != 3) continue;
+                var airport = new Airport
+                {
+                    Identifier = splits[0],
+                    NumberOfVotes = ushort.Parse(splits[2]),
+                    MaximumAircraftPerHour = (ushort)Math.Round(double.Parse(splits[1]) / 3),
+                };
+
+                airports.Add(airport.Identifier, airport);
+                locations.Add(airport.Identifier, airport);
+
+                vatsimEvent.Airports.Add(airport);
+            }
+
+            // read route segments
+            foreach (var line in File.ReadAllLines(Path.Combine("TestingData", $"{eventPrefix} RouteSegments.csv")))
+            {
+                var splits = line.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                if (splits.Length != 3) continue;
+                var routeSegment = new RouteSegment() { Identifier = splits[0], RouteString = splits[1], RouteSegmentGroup = splits[2] };
+                foreach (var waypoint in splits[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+                {
+                    if (Char.IsDigit(waypoint.Last()) || waypoint == "DCT") continue; // exclude airways and directs
+                    if (!locations.TryGetValue(waypoint, out var location))
+                    {
+                        location = new Location() { Identifier = waypoint };
+                        locations.Add(waypoint, location);
+                    }
+                    routeSegment.Locations.Add(location);
+                    vatsimEvent.Waypoints.Add(location);
+                }
+                vatsimEvent.RouteSegments.Add(routeSegment);
+            }
+
+            return vatsimEvent;
+        }
+    }
+}
